a71408e0d8814e50819e98dabf10d25dcb9ab3c7
Jean <jean118877@hotmail.com>
2023-02-11 21:05:08 -0300

    fix: text

--- components/Card.tsx
+++ components/Card.tsx
@@ -14,7 +14,7 @@ export default function Card({pokemon}: {pokemon: pokemon}){
       <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`} alt={pokemon.name} />
       <h1>{pokemon.name}</h1>
       <Link href={`/pokemon/${pokemon.id}`} className={styles['link']}>
-        Descrição
+        Detalhes
       </Link>
     </div>
   );
